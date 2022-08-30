7fee1e671439555d4b3830cd3a540527ca3346c8
hanshan <hanshan@Ubuntu20.04>
2022-08-30 11:44:26 +0800

    feat(all): init repo

---
+++ chapter3/Datademo02.java
@@ -0,0 +1,8 @@
+public class Datademo02 {
+  public static void main(String[] arg) {
+    int max = Integer.MAX_VALUE;
+    System.out.println("整型的最大数:" + max);
+    System.out.println("整型最大数加1:" + (max + 1));
+    System.out.println("整型最大数加2:" + (max + 2));
+  }
+}
---
+++ chapter3/Datademo06.java
@@ -0,0 +1,6 @@
+public class Datademo06{
+  public static void main(String[] arg) {
+    float num = 3;
+    System.out.println("num*num=" + num * num);
+  }
+}
